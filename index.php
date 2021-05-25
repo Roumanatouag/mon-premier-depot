0f355b7bc8c2d288d332bc2e5b5f34d490dfa09a
Roumanatouag <84721521+Roumanatouag@users.noreply.github.com>
2021-05-25 11:10:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>School Project</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.css" />
+   
+</head>
+<body>
+<?php
+       require_once("traitements_php/connexion_bdd.php");
+?>
+    <div><h1> Bienvenue sur le site web school project  </div></div>
+    <div class="form-group"> 
+        <a href="login.php" class="btn btn-lg btn-primary">Login</a>
+    </div> 
+    <div class="form-group"> 
+        <a href="typepage.php" class="btn btn-lg btn-primary">Inscription</a>
+    </div> 
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
